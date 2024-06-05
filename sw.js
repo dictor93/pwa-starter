181e64166cef793a0fb4049750c601b92e6a4b9e
dictor93 <dictor93@gmail.com>
2024-06-05 23:29:30 +0300

    Updated serviceworker

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
 "use strict";
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installing.');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activating.');
+})
 // importScripts('/pwa-starter/sw-toolbox.js');
 // toolbox.precache(['index.html','style/style.css']);
 // toolbox.router.get('/images/*', toolbox.cacheFirst);
